56591a6e7cefacba4cf404902ea7ce1f702bf8d6
WAF-wearefamily <43478338+WAF-wearefamily@users.noreply.github.com>
2018-10-17 12:44:38 +0200

    removed rss parsing
    Switched to IFTT, errors due to Heroku free plan, may add later rss fetching again

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const Telegraf = require('telegraf');
-let Parser = require('rss-parser');
-let parser = new Parser();
+//let Parser = require('rss-parser');
+//let parser = new Parser();
 
 const bot = new Telegraf('671026420:AAG3Vx5anaNoCKCMF0z_JN-fGQWJk2gW9kE');
 
 
 bot.start((ctx) => ctx.reply('Ciao! Sono Infoumy il bot di Informatica Umanistica.\nAll\' interno del gruppo telegram posterò in automatico tutte le news pubblicate dai docenti sul sito del corso e gli aggiornamenti del drive del corso.\nQui puoi chattare con me, per la lista dei comandi disponibili digita o clicca /help.'))
 bot.help((ctx) => ctx.reply('Puoi interagire con me scrivendo le seguenti stringhe in chat privata:\nappunti : mostra il link al drive\norario : mostra il link all\'orario\nfeed: stampa le ultime news del corso\nftp: stampa le news di Fondamenti'))
+bot.hears('/help@InfoUmaBot', (ctx) => ctx.reply('Puoi interagire con me scrivendo le seguenti stringhe in chat privata:\nappunti : mostra il link al drive\norario : mostra il link all\'orario\nfeed: stampa le ultime news del corso\nftp: stampa le news di Fondamenti'));
 bot.hears('infoumy', (ctx) => ctx.reply('Hey ciao usa /help per sapere quali sono i miei comandi!'))
 bot.hears('appunti', (ctx) => ctx.reply('Drive: https://drive.google.com/open?id=1ER_GYyWSPvwinWuHiisAVq7IlJwK2E6P'))
 bot.hears('orario', (ctx) => ctx.reply('Orario: http://www.fileli.unipi.it/infouma/laurea-triennale/orario/'))
 
-bot.hears('feed', async (ctx) => {
+/*bot.hears('feed', async (ctx) => {
 
   let feed = await parser.parseURL("http://www.fileli.unipi.it/infouma/category/home/feed/");
   ctx.reply(feed.title);
@@ -41,3 +42,5 @@ bot.hears('ftp', async (ctx) => {
   });
 
 });
+*/
+bot.startPolling()
